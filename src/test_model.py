dce7c61293828fa71b33e284a6e2c1d4dba899c9
RiccardoBuzzoni <riccardobuzzoni01@gmail.com>
2025-07-08 14:18:55 +0200

    Aggiunto test per caricamento modello

---
+++ src/test_model.py
@@ -0,0 +1,14 @@
+from src.utils import load_model
+import os
+
+def test_model_loading():
+    # Verifica che il modello possa essere caricato correttamente
+    if not os.path.exists("artifacts/model"):
+        print("Modello non presente. Salvalo prima con train.py")
+        assert False
+    try:
+        model = load_model()
+        assert model is not None
+    except Exception as e:
+        print(f"Errore durante il caricamento del modello: {e}")
+        assert False
